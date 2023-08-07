13ba403afc925f9deaacfeadcc699c59ecac13f7
Kayky_CN <84750720+Kcioban@users.noreply.github.com>
2023-08-07 16:10:43 -0300

    Update CalcTabuada.c

--- Project/CalcTabuada.c
+++ Project/CalcTabuada.c
@@ -1,18 +1,18 @@
-#include <stdio.h>
-
-int main() {
-    // definindo variavel
-    int i, numero;
-
-    printf("\nEscolha um numero para receber a tabuada dele.\n");
-    scanf("%d", &numero);
-
-    // plot da tabuada
-    printf("\nTabuada do %d\n\n", numero);
-    for (i = 1; i <= 10; i++)
-    {
-        // calculo da tabuada
-        printf("%d x %d = %d\n", numero, i, numero * i);
+    #include <stdio.h>
+    
+    int main() {
+        // definindo variavel
+        int i, numero;
+    
+        printf("\nEscolha um numero para receber a tabuada dele.\n");
+        scanf("%d", &numero);
+    
+        // plot da tabuada
+        printf("\nTabuada do %d\n\n", numero);
+        for (i = 1; i <= 10; i++)
+        {
+            // calculo da tabuada
+            printf("%d x %02d = %02d\n", numero, i, numero * i);
+        }
+        return 0;
     }
-    return 0;
-}
